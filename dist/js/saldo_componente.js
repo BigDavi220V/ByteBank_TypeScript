28b5678c5ace060afdb7e719673d251aeeb1bc9f
Davi Santiago <davi_santiagoo@hotmail.com>
2025-11-01 22:13:17 -0300

    arquivo FormatoData e formatters criados em src e a pagina saldo_componente está sendo refatorado

--- dist/js/saldo_componente.js
+++ dist/js/saldo_componente.js
@@ -1,6 +1,10 @@
 let saldo = 3000;
-alert("testando ts");
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = formatarMoeda(saldo);
+}
+if (elementoDataAcesso != null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
 }
