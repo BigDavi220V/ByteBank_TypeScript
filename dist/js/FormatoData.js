d4d6e5d1fa012e42887a68adc6bdfc9d99f6206a
BigDavi220V <davi_santiagoo@hotmail.com>
2025-11-03 10:34:26 -0300

    instalacao do typescript

---
+++ dist/js/FormatoData.js
@@ -0,0 +1,6 @@
+var FormatoData;
+(function (FormatoData) {
+    FormatoData["PADRAO"] = "DD/MM/AAAA";
+    FormatoData["DIA_SEMANA_DIA_ANO"] = "DIA_SEMANA, DD/MM/AAAA";
+    FormatoData["DIA_MES"] = "DD/MM";
+})(FormatoData || (FormatoData = {}));
